62b175cdbb38941df8f737bb83de5b07f113c9c0
Devika Sharma <136916439+devikasharma1234@users.noreply.github.com>
2026-09-21 10:42:22 +0530

    Time: 0 ms (100%), Space: 44.7 MB (94.62%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         if(nums.length == 1) return true;
-        int first = 0;
-        int second = 1;
 
         for(int i=0; i<nums.length-1; i++){
-            if(!((nums[first]%2 == 0 && nums[second]%2 != 0) || (nums[first] %2 != 0 && nums[second]%2 == 0))){
+            if(nums[i] % 2 == nums[i+1] % 2){ 
+                // check whether if two consecutive number are either odd or even
                 return false;
             }
-            first++;
-            second++;
         }
         return true;
     }
